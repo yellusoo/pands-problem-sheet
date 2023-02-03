2ee36db4954e9f2f54ecfbf4dd018c4107176bd0
unknown <oosulley@gmail.com>
2023-02-03 19:11:38 +0000

    Topic 1 Redo 3rd Feb

---
+++ bank.py
@@ -0,0 +1,6 @@
+#Week 2 Task -
+#Author Oisin O Sullivan
+git config --global user.email "oosulley@gmail.com"
+  git config --global user.name "Oisin"
+print("Hello")
+input = "Enter your first amount here "
